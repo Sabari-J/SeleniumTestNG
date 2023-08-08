701ea1501301e4802899f64b11c5982337adfd71
sabari <sabari.sun7@gmail.com>
2023-08-08 16:28:54 +0530

    Date Picker 2

--- src/test/java/PageObjClasses/DatePicker.java
+++ src/test/java/PageObjClasses/DatePicker.java
@@ -1,12 +1,23 @@
 package PageObjClasses;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
 import Base.base;
 
-public class DatePicker extends base{
-	
+public class DatePicker extends base {
+
+	AccordianOptions acdnOptns = new AccordianOptions();
+
+	@Test(priority = 0)
 	public void clickOnDatePicker() {
-		
+		acdnOptns.clickOnWidgets();
+
+		WebElement datePicker = driver.findElement(By.xpath("//span[.='Date Picker']"));
+		scrollToElement(datePicker);
+		datePicker.click();
+
 	}
-	
 
 }
