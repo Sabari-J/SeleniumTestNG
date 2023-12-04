3d4c1e07dd80a91b273ab1e2cdd8c51bbcc40452
sabari <sabari.sun7@gmail.com>
2023-12-04 12:18:10 +0530

    Droppable - END

--- src/test/java/PageObjClasses/Droppable.java
+++ src/test/java/PageObjClasses/Droppable.java
@@ -2,7 +2,6 @@ package PageObjClasses;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 import Base.base;
@@ -38,8 +37,8 @@ public class Droppable extends base {
 		acceptTab.click();
 		System.out.println("Successfully clicked on Accept Tab");
 
-		 String option = "acceptable";
-		//String option = "notacceptable";
+		String option = "acceptable";
+		// String option = "notacceptable";
 		WebElement dropHereBox = driver
 				.findElement(By.xpath("//div[@id='acceptDropContainer']//child::p[.='Drop here']"));
 		if (option.equalsIgnoreCase("Acceptable")) {
@@ -66,45 +65,67 @@ public class Droppable extends base {
 		WebElement dragMeBox = driver.findElement(By.xpath("//div[@id='ppDropContainer']//child::div[@id='dragBox']"));
 
 		String greedyOption = "not greedy";
-		//String greedyOption = "greedy";
+		// String greedyOption = "greedy";
 
 		// notGreedyDropBox
 		if (greedyOption.equalsIgnoreCase("not greedy")) {
-			//String option = "inner droppable"; - //div[@id='notGreedyInnerDropBox' and .='Inner droppable (not greedy)']
-			 String option = "outer droppable";
+			// String option = "inner droppable";
+			String option = "outer droppable";
 
 			if (option.equalsIgnoreCase("outer droppable")) {
 				WebElement outerDroppableBox = driver
-						.findElement(By.xpath("//child::p[.='Outer droppable']"));
+						.findElement(By.xpath("//div[@id='notGreedyDropBox']//child::p[.='Outer droppable']"));
 				dragAndDropElement(dragMeBox, outerDroppableBox);
-				System.out.println("Dropped into the outer box Successfully ");
+				System.out.println("Dropped into the NotGreedy_Outer box Successfully ");
 
 			} else if (option.equalsIgnoreCase("inner droppable")) {
-				WebElement innerDroppableBox = driver.findElement(
-						By.xpath("//child::div[.='Inner droppable (not greedy)']"));
+				WebElement innerDroppableBox = driver
+						.findElement(By.xpath("//child::div[.='Inner droppable (not greedy)']"));
 				dragAndDropElement(dragMeBox, innerDroppableBox);
-				System.out.println("Dropped into the inner box Successfully");
+				System.out.println("Dropped into the NotGreedy_inner box Successfully");
 			}
 
 			// GreedyDropBox
 		} else if (greedyOption.equalsIgnoreCase("greedy")) {
 
-			 String option = "inner droppable";
-			 //String option = "outer droppable";
+			// String option = "inner droppable";
+			String option = "outer droppable";
 			if (option.equalsIgnoreCase("outer droppable")) {
 				WebElement outerDroppableBox = driver
-						.findElement(By.xpath("//child::p[.='Outer droppable']"));
+						.findElement(By.xpath("//div[@id='greedyDropBox']//child::p[.='Outer droppable']"));
 				dragAndDropElement(dragMeBox, outerDroppableBox);
-				System.out.println("Dropped into the outer box Successfully");
+				System.out.println("Dropped into the Greedy_Outer box Successfully");
 
 			} else if (option.equalsIgnoreCase("inner droppable")) {
 				WebElement innerDroppableBox = driver
 						.findElement(By.xpath("//child::div[.='Inner droppable (greedy)']"));
 				dragAndDropElement(dragMeBox, innerDroppableBox);
-				System.out.println("Dropped into the inner box Successfully");
+				System.out.println("Dropped into the Greedy_inner box Successfully");
 			}
 
 		}
 
+	}
+
+	@Test(priority = 4)
+	public void revertDraggable() {
+		WebElement revertDraggableTab = driver
+				.findElement(By.xpath("//a[contains(@id, 'revertable') and .='Revert Draggable']"));
+		revertDraggableTab.click();
+		//String revertOption = "Will Revert";
+		 String revertOption = "Not Revert";
+
+		WebElement dropHereBox = driver
+				.findElement(By.xpath("//div[contains(@id,'revertable')]//child::div[.='Drop here']"));
+
+		if (revertOption.equalsIgnoreCase("will Revert")) {
+			WebElement willRevertBox = driver.findElement(By.xpath("//div[@id='revertable' and .='Will Revert']"));
+			dragAndDropElement(willRevertBox, dropHereBox);
+			System.out.println("Succesfully dropped the Will Revert into drop here Box");
+		} else if (revertOption.equalsIgnoreCase("Not Revert")) {
+			WebElement notRevertBox = driver.findElement(By.xpath("//div[@id='notRevertable' and .='Not Revert']"));
+			dragAndDropElement(notRevertBox, dropHereBox);
+			System.out.println("Succesfully dropped the Not Revert into drop here Box");
+		}
 	}
 }
